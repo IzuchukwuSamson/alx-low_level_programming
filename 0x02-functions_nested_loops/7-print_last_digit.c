03ba2d3844ad1ba9338d074b0b44aeebc5fe776d
Izuchukwu Samson <81382764+IzuchukwuSamson@users.noreply.github.com>
2022-03-15 14:12:47 +0100

    A new commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,20 @@
-#include "main.h"
+#include "main.c"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * print_last_digit - prints the last digit of a number
+ *@n: number being tested
  * Return: Always 0.
  */
-int main(void)
-{
-	int r;
 
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
-	return (0);
+int print_last_digit(int n)
+{
+	int lastDig;
+	if (n >= 0) {
+		lastDig = n % 10;
+	}
+	else {
+		lastDig = (n % 10) * -1;
+	}
+	_putchar('0' + lastDig);
+	return(lastDig)
 }
